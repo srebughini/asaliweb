936dfae24fd6683fbf45cd33e8109c5f61e89263
srebughini <ste.rebu@gmail.com>
2020-12-01 23:00:08 +0100

    Trying to use async

--- js/testing/asali.js
+++ js/testing/asali.js
@@ -1,30 +1,127 @@
-function loadJSON(jsonFilePath) {   
-
-    let requestObj = new XMLHttpRequest();
-    let parsedJson = {}
-    requestObj.overrideMimeType("application/json");
-    requestObj.open('GET', jsonFilePath, true);
-
-    console.log(requestObj);
-
-    console.log(requestObj.readyState);
-    console.log(requestObj.status);
-
-    /*console.log(callback(requestObj.responseText));
-
-    if (requestObj.readyState == 4 && requestObj.status == "200") {
-    parsedJson = JSON.parse(requestObj.responseText);
-    }
-    requestObj.send(null);
-    return parsedJson;*/
- }
+var OMEGA_FILE_PATH = "js/testing/data/omega.json"
+var THERMO_FILE_PATH = "js/testing/data/thermo.json"
+var TRANSPORT_FILE_PATH = "js/testing/data/transport.json"
 
 /*
+function loadJsonFile(callback, jsonFilePath) {
+    let requestObj = new XMLHttpRequest();
+    requestObj.overrideMimeType("application/json");
+    requestObj.open('GET', jsonFilePath, true);
+    requestObj.onload = function (e) {
+        if (requestObj.readyState === 4) {
+          if (requestObj.status === 200) {
+            callback(requestObj.responseText);
+          } else {
+            console.error(requestObj.statusText);
+          }
+        }
+      };
+      requestObj.onerror = function (e) {
+        console.error(requestObj.statusText);
+      };
+
+    requestObj.send(null);
+    //return JSON.parse(requestObj.responseText);
+}
+
+function stringToJson(message) {
+    return JSON.parse(this.responseText);
+}
+*/
+
+function loadJsonFile(url, callback /*, opt_arg1, opt_arg2, ... */) {
+    var xhr = new XMLHttpRequest();
+    xhr.overrideMimeType("application/json");
+    xhr.callback = callback;
+    xhr.arguments = Array.prototype.slice.call(arguments, 2);
+    xhr.onload = function () { this.callback.apply(this, this.arguments); };
+    xhr.onerror = function () { console.error(this.statusText); };
+    xhr.open("GET", url, true);
+    xhr.send(null);
+}
+
 const Fractions = Object.freeze({ MOLE: "mole", MASS: "mass" });
 
-const ThermoParameters = (coefficientArray) => {
-    let _highTemperatureCoefficients = coefficientArray.slice(0, 7);
-    let _lowTemperatureCoefficients = coefficientArray.slice(7, 14);
+const ThermoParameters = () => {
+    let _thermoDict = {}
+
+    loadJsonFile(THERMO_FILE_PATH, _setThermoDict);
+
+    function _setThermoDict()
+    {
+        _thermoDict = this.responseText;
+    }
+
+    function getHighTemperatureCoefficients(gasSpecieName) {
+        return _thermoDict[gasSpecieName].slice(0, 7);
+    }
+
+    function getLowTemperatureCoefficients(gasSpecieName) {
+        return _thermoDict[gasSpecieName].slice(7, 14);
+    }
+
+    return { getHighTemperatureCoefficients, getLowTemperatureCoefficients }
+}
+
+const TransportParameters = () => {
+    let _transportDict = {}
+    loadJsonFile(TRANSPORT_FILE_PATH, _setTransportDict);
+    
+    function _setTransportDict()
+    {
+        _transportDict = this.responseText;
+    }
+
+    async function getGeometry(gasSpecieName) {
+        return _transportDict[gasSpecieName][0];
+    }
+
+    async function getLJpotential(gasSpecieName) {
+        return _transportDict[gasSpecieName][1];
+    }
+
+    async function getLJdiameter(gasSpecieName) {
+        return _transportDict[gasSpecieName][2];
+    }
+
+    async function getDipole(gasSpecieName) {
+        return _transportDict[gasSpecieName][3];
+    }
+
+    async function getPolar(gasSpecieName) {
+        return _transportDict[gasSpecieName][4];
+    }
+
+    async function getCollision(gasSpecieName) {
+        return _transportDict[gasSpecieName][5];
+    }
+
+    async function getMolecularWeight(gasSpecieName) {
+        return _transportDict[gasSpecieName][6];
+    }
+
+    return { getGeometry, getLJpotential, getLJdiameter, getDipole, getPolar, getCollision, getMolecularWeight }
+}
+
+const GasSpecie = ({ name, transportParameters, thermoParameters }) => {
+    let _name = name;
+    let _highTemperatureCoefficients = thermoParameters.getHighTemperatureCoefficients(_name);
+    let _lowTemperatureCoefficients = thermoParameters.getLowTemperatureCoefficients(_name);
+    let _geometry = transportParameters.getGeometry(_name);
+    let _LJpotential = transportParameters.getLJpotential(_name);
+    let _LJdiameter = transportParameters.getLJdiameter(_name);
+    let _dipole = transportParameters.getDipole(_name);
+    let _polar = transportParameters.getPolar(_name);
+    let _collision = transportParameters.getCollision(_name);
+    let _molecularWeight = transportParameters.getMolecularWeight(_name);
+
+    function getName() {
+        return _name;
+    }
+
+    function getMolecularWeight() {
+        return _molecularWeight;
+    }
 
     function getHighTemperatureCoefficients() {
         return _highTemperatureCoefficients;
@@ -33,16 +130,6 @@ const ThermoParameters = (coefficientArray) => {
     function getLowTemperatureCoefficients() {
         return _lowTemperatureCoefficients;
     }
-
-    return { getHighTemperatureCoefficients, getLowTemperatureCoefficients }
-}
-
-const TransportParameters = ({ geometry, LJpotential, LJdiameter, dipole, polar }) => {
-    let _geometry = geometry;
-    let _LJpotential = LJpotential;
-    let _LJdiameter = LJdiameter;
-    let _dipole = dipole;
-    let _polar = polar;
 
     function getGeometry() {
         return _geometry;
@@ -64,32 +151,11 @@ const TransportParameters = ({ geometry, LJpotential, LJdiameter, dipole, polar 
         return _polar;
     }
 
-    return { getGeometry, getLJpotential, getLJdiameter, getDipole, getPolar }
-}
-
-const GasSpecie = ({ name, molecularWeight, transportParameters, thermoParameters }) => {
-    let _name = name;
-    let _molecularWeight = molecularWeight;
-    let _transportParameters = transportParameters;
-    let _thermoParameters = thermoParameters;
-
-    function getName() {
-        return _name;
+    function getCollision() {
+        return _collision;
     }
 
-    function getMolecularWeight() {
-        return _molecularWeight;
-    }
-
-    function getTransportParameters() {
-        return _transportParameters;
-    }
-
-    function getThermoParameters() {
-        return _thermoParameters;
-    }
-
-    return { getName, getMolecularWeight, getThermoParameters, getTransportParameters }
+    return { getName, getMolecularWeight, getHighTemperatureCoefficients, getLowTemperatureCoefficients, getGeometry, getLJpotential, getLJdiameter, getDipole, getPolar, getCollision }
 }
 
 const GasState = ({ temperature, pressure }) => {
@@ -169,44 +235,26 @@ const GasMixture = ({ gasState, mixtureComposition }) => {
 
     return {}
 }
-*/
+
+thermo = ThermoParameters()
+transport = TransportParameters()
+
+
+console.log(thermo.getHighTemperatureCoefficients("H2"))
 
 /*
-function init() {
-    loadJSON(function(response) {
-     // Parse JSON string into object
-        console.log(response);
-       var actual_JSON = JSON.parse(response);
-       console.log(actual_JSON);
-    });
-   }
+h2_gas = GasSpecie({ name: "H2", thermoParameters: thermo, transportParameters: transport })
+o2_gas = GasSpecie({ name: "O2", thermoParameters: thermo, transportParameters: transport })
+n2_gas = GasSpecie({ name: "N2", thermoParameters: thermo, transportParameters: transport })
 
-   init();
-*/
+console.log(h2_gas.getMolecularWeight())
+console.log(n2_gas.getMolecularWeight())
+console.log(o2_gas.getHighTemperatureCoefficients())
 
-var a = loadJSON("js/testing/data/omega.json");
-
-console.log(a);
-
-//var actual_JSON = JSON.parse(a);
-
-//console.log(actual_JSON)
-
-
-
-
-/*
-thermo = ThermoParameters([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17])
-transport = TransportParameters({ geometry: 1, LJpotential: 2, LJdiameter: 3, dipole: 2, polar: 0 })
-
-h2_gas = GasSpecie({ name: "H2", molecularWeight: 2., thermoParameters: thermo, transportParameters: transport })
-o2_gas = GasSpecie({ name: "O2", molecularWeight: 32., thermoParameters: thermo, transportParameters: transport })
-n2_gas = GasSpecie({ name: "N2", molecularWeight: 28., thermoParameters: thermo, transportParameters: transport })
 
 state = GasState({ temperature: 298.15, pressure: 101325 })
 
 compositions = GasMixtureComposition([{ "specie": h2_gas, "value": 0.1 }, { "specie": o2_gas, "value": 0.2 }, { "specie": n2_gas, "value": 0.7 }], "mole")
 
 
-mixture = GasMixture({ gasState: state, mixtureComposition: compositions })
-*/
+mixture = GasMixture({ gasState: state, mixtureComposition: compositions })*/
